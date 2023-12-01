7abd90a9c2462cfadd7bee53bf74823eec7bfe7a
kerimamansaryyev <keamansaryyev@gmail.com>
2023-12-01 04:48:19 -0600

    Completed lab2

--- src/com/algorithms/lab2/Algorithm.java
+++ src/com/algorithms/lab2/Algorithm.java
@@ -12,7 +12,7 @@ public interface Algorithm {
                 new Algorithm2()
         };
 
-        final int[] basicInput = new int[]{7, 20, 18, 4, 20, 19, 20, 3};
+        final int[] basicInput = new int[]{6,10,1,2};
 
         for(final var algo: algorithms){
             System.out.println("Executing: " + algo.getName());
@@ -36,24 +36,20 @@ class Algorithm2 implements  Algorithm {
         }
         int max = Integer.MIN_VALUE, preMax = Integer.MIN_VALUE, prePreMax = Integer.MIN_VALUE;
 
-        // n for number of comparisons under the enhanced loop
-        // 2n for index traversing under the enhanced loop
+
         for (int i=0;i<array.length;i++) {
-            // n times array[i] has been accessed
             int number = array[i];
-            // n for comparison
+
             if (number > max) {
-                // n for assignment
+                prePreMax = preMax;
+                preMax = max;
                 max = number;
             }
-            // (n-1) for comparisons
-            else if (number > preMax) {
-                // (n-1) for assignment
+            else if(number > preMax){
+                prePreMax = preMax;
                 preMax = number;
-
-                // (n-2) for comparisons
-            } else if (number > prePreMax) {
-                // (n-2) for assignments
+            }
+            else if(number > prePreMax){
                 prePreMax = number;
             }
         }
@@ -78,20 +74,16 @@ class Algorithm1 implements  Algorithm {
 
         int max = Integer.MIN_VALUE;
 
-        // i=0 is 1
-        // i<array.length is n
-        // i++ is 2n
+
         for(int i=0;i<array.length;i++){
-            // array[i] is n
-            // array[i] > max is n
             if(array[i] > max){
-                // assignment + index = 2n
+
                 max = array[i];
-                // assignment is n
+
                 maxIndex = i;
             }
         }
-        // Total: 8n for this loop
+
 
         max = Integer.MIN_VALUE;
 
